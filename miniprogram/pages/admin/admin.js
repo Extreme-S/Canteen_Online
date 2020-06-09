307b0c27d2a30bdae8ab3e552613cb0b3651bfac
叶博涛 <beating_heart@yeah.net>
2020-06-09 18:32:32 +0800

    优化业务逻辑
    1. 增加了管理员界面，查看个人信息的入口。

--- miniprogram/pages/admin/admin.js
+++ miniprogram/pages/admin/admin.js
@@ -2,10 +2,34 @@ const app = getApp()
 Page({
   data: {
     value: "",
-
-    notice2: [{}],
+    show: false,
     
   },
   
+  onLoad(){
+    var that = this;
+    wx.getUserInfo({
+      success: function(res) {
+        //console.log(res);
+        that.setData({
+          'user_info.nickName': res.userInfo.nickName,
+          'user_info.avatarUrl': res.userInfo.avatarUrl,
+        })
+      }
+    })
+  },
+
+
+  showPopup() {
+    this.setData({
+      show: true
+    });
+  },
+
+  onClose() {
+    this.setData({
+      show: false
+    });
+  },
 })
 
